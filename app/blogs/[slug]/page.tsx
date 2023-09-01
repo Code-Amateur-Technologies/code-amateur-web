205f6a30b0ee64c6bf6fcd99d2bebc35a9410773
TarunShahiWork <tarun.shahi@codeamateur.com>
2023-09-01 18:01:49 +0530

    500 issue fix test

--- app/blogs/[slug]/page.tsx
+++ app/blogs/[slug]/page.tsx
@@ -1,27 +1,34 @@
-// import axios from "axios";
 import slugify from "react-slugify";
 import { JSDOM } from "jsdom";
 import DOMPurify from "dompurify";
 import { GetBlogs } from "@/utils/GetBlogs";
-import { Metadata, ResolvingMetadata } from 'next'
+import { Metadata } from 'next'
 
-export async function generateMetadata(
-  { params }: {
-    params: { slug: string };
-  },
-): Promise<Metadata> {
-  // read route params
-  const linkTitle = params?.slug;
-  let metaTitle: string = ""
-  const data = await GetBlogs()
-  data.map((blog: any) =>
-    slugify(blog.title) === linkTitle ? metaTitle = blog.title : ""
-  );
+// export async function generateMetadata(
+//   { params }: {
+//     params: { slug: string };
+//   },
+// ): Promise<Metadata> {
+//   const linkTitle = params?.slug;
+//   let metaData: any = []
+//   const data = await GetBlogs()
+//   data.map((blog: any) =>
+//     slugify(blog.title) === linkTitle ? metaData.push(...blog) : ""
+//   );
  
-  return {
-    title: "Code Amateur | Blog: " + metaTitle ,
-  }
-}
+//   return {
+//     metadataBase: new URL('https://codeamateur.netlify.app/blogs/'),
+//     title: "Code Amateur | Blog: " + metaData.title,
+//     description: metaData.title,
+//     openGraph: {
+//       title: metaData.title,
+//       description: metaData.title,
+//       siteName: "CAT",
+//       images: metaData.thumbnail,
+//       type: 'website',
+//     },
+//   }
+// }
 
 interface IBlog {
   description: string;
@@ -38,16 +45,15 @@ export default async function BlogPost({
 }) {
   const linkTitle = params?.slug;
   const blogs = await GetBlogs();
-  let blogArray: any = [];
-  blogs.map((blog: any) =>
-    slugify(blog.title) === linkTitle ? blogArray.push({ ...blog }) : ""
+  const article: any = blogs.find((blog: any) =>
+    slugify(blog.title) === linkTitle
   );
-  console.log("Tarun-Blogs: ", blogArray[0].title);
+  console.log("Tarun-Blogs: ", article.title);
   const window = new JSDOM("").window;
   const purify = DOMPurify(window);
-  const clean = purify.sanitize(blogArray[0].description);
+  const clean = purify.sanitize(article.description);
   // const blogContent = DOMPurify.sanitize(blogArray[0].description);
-  const date: any = blogArray[0].pubDate.split(" ");
+  const date: any = article.pubDate.split(" ");
 
   // const [blogData, setBlogData] = useState<IBlog>({
   //   description: "",
@@ -77,19 +83,19 @@ export default async function BlogPost({
     <div>
       <section className="h-full py-8 px-4 sm:p-20 border-b flex-between">
         <h1 className="w-1/2 text-6xl font-medium text-gunmetal">
-          {blogArray[0].title}
+          {article.title}
         </h1>
       </section>
       <section className="px-20">
         <div className="py-10 flex gap-5 text-xl font-bold text-gunmetal">
-          <p>Author: {blogArray[0].author}</p>
+          <p>Author: {article.author}</p>
           <p>Date: {date[0]}</p>
         </div>
         <div>
           <div className="text-xl" dangerouslySetInnerHTML={{ __html: clean }} />
         </div>
         <div className="py-10 flex gap-2">
-          {blogArray[0].categories.map((items: any) => (
+          {article.categories.map((items: any) => (
             <div key={items} className="blog-category-chip">
               {items}
             </div>
