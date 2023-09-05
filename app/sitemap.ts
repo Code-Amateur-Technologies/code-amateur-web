c19027bbdab345c51529687306eee9a2e67f9a0e
TarunShahiWork <tarun.shahi@codeamateur.com>
2023-09-05 18:53:58 +0530

    removed changefrequency and priority

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,32 +5,22 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: "https://codeamateur.com/",
       lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 1,
     },
     {
       url: "https://codeamateur.com/services",
       lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
     },
     {
       url: "https://codeamateur.com/blogs",
       lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.5,
     },
     {
       url: "https://codeamateur.com/about",
       lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.5,
     },
     {
       url: "https://codeamateur.com/contact",
       lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.5,
     },
   ];
 }
