d42ceac2c9d81b88dc5e48cb184e8a9a8b346104
Lancy Goyal <i.lancygoyal@gmail.com>
2023-09-05 22:59:03 +0530

    Development (#8)
    * Feature/seo (#5)
    
    * seo issues fixed
    
    * css bug removed
    
    * Implemented contact form
    
    * Contact form captcha integration
    
    * Contact form captcha implementation 2
    
    * Disable button until captcha verify and showing our medium feed
    
    * Vercel Analytics added
    
    * removed changefrequency and priority
    
    ---------
    
    Co-authored-by: TarunShahiWork <tarun.shahi@codeamateur.com>
    
    * minor fix
    
    * Update GetBlogs.tsx
    
    ---------
    
    Co-authored-by: TarunShahiWork <tarun.shahi@codeamateur.com>

--- components/GetBlogs.tsx
+++ components/GetBlogs.tsx
@@ -1,7 +1,7 @@
 import slugify from "react-slugify";
 
 export async function GetBlogs() {
-  const Link = `https://api.rss2json.com/v1/api.json?rss_url=https://codeamateur.medium.com/feed`;
+  const Link = `https://api.rss2json.com/v1/api.json?rss_url=https://codeamateur.medium.com/feed&api_key=esqmehbyut9mfcvjyz1vflvypn9siuvharniofwz&order_by=pubDate&count=25`;
   const mediumFeed = await fetch(Link)
     .then((response) => response.json())
     .then((data) => {
