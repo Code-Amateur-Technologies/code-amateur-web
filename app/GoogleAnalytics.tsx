a250b192d1948a491c0c55d5f4e0cecc89a7340a
TarunShahiWork <tarun.shahi@codeamateur.com>
2023-09-06 15:59:52 +0530

    changed google analytics key

--- app/GoogleAnalytics.tsx
+++ app/GoogleAnalytics.tsx
@@ -7,14 +7,14 @@ const GoogleAnalytics = () => {
     <>
       <Script
         async
-        src="https://www.googletagmanager.com/gtag/js?id=G-8N4P066RRQ"
+        src="https://www.googletagmanager.com/gtag/js?id=G-1ZP3R3MLJC"
         strategy="afterInteractive"
       ></Script>
       <Script id="google-analytics" strategy="afterInteractive">
         {`window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
-        gtag('config', 'G-8N4P066RRQ');`}
+        gtag('config', 'G-1ZP3R3MLJC');`}
       </Script>
     </>
   );
