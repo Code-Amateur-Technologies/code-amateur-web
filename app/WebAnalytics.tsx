0f78a6084a5a6f344b83d87a455d630662c2e9d3
TarunShahiWork <tarun.shahi@codeamateur.com>
2023-09-06 15:13:41 +0530

    removed console.log from analytics

--- app/WebAnalytics.tsx
+++ app/WebAnalytics.tsx
@@ -2,7 +2,6 @@ import GoogleAnalytics from "./GoogleAnalytics";
 import { Analytics } from "@vercel/analytics/react";
 
 export default function WebAnalytics() {
-//   console.log("enviorment testing tag", process.env.NODE_ENV);
   return (
     <>
       {process.env.NODE_ENV === "production" && (
